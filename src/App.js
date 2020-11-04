915952e398c1935d18dcc35691263aaa85446a61
dleroy <david_leroy@intuit.com>
2020-11-04 13:41:15 -0800

    Refactor routes and moved useGridData hook to App

--- src/App.js
+++ src/App.js
@@ -2,22 +2,40 @@ import './App.css';
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import TopToolBar from './Components/TopToolBar';
-import routes from './routes.js';
+import useGridData from './Hooks/useGridData';
+import DataImporter from './Components/DataImporter';
+import Home from './Components/Home';
+import GridView from './Components/GridView';
+import CapacityRow from './Components/CapacityRow';
 
 export default function App() {
+    const {gridData, setGridData} = useGridData();
+
     return (
         <BrowserRouter>
             <TopToolBar />
                 <Switch>
-                    {routes.map((route, index) => (
-                        <Route
-                        key={index}
-                        path={route.path}
-                        component={route.component}
-                        render={route.render}
+                    <Route path="/loadData">
+                        <DataImporter />
+                    </Route>
+                    <Route path="/gridView">
+                        <GridView
+                            gridData={gridData}
+                            setGridData={setGridData}
+                            />
+                    </Route>
+                    <Route path="/capacity">
+                        <CapacityRow
+                            gridData={gridData}
+                            setGridData={setGridData}
+                            />
+                    </Route>
+                    <Route path="/">
+                        <Home
+                            gridData={gridData}
+                            setGridData={setGridData}
                         />
-
-                    ))}
+                    </Route>
                 </Switch>
         </BrowserRouter>
     );
